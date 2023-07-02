ee59f00c806a9a97cf66603f491a6ee4cf75259c
xuanngoc2k2 <xuanngoc2k2@gmail.com>
2023-07-03 03:36:54 +0700

    Footer

--- src/layout/Components/Footer/index.js
+++ src/layout/Components/Footer/index.js
@@ -5,7 +5,20 @@ import styles from './Footer.module.scss'
 const cx = classNames.bind(styles)
 function Footer() {
     return (
-        <div className={cx('wrapper-footer')}><h1>Footer</h1></div>);
+        <div className={cx('wrapper-footer')}>
+            <div className={cx('socialFoot')}>
+
+            </div>
+            <div className={cx('copyRight')}>
+
+            </div>
+            <div className={cx('linkFooter')}>
+
+            </div>
+            <div className={cx('linkMb')}>
+
+            </div>
+        </div>);
 }
 
 export default Footer;
